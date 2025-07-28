41ab53b594ca0a25a9391955d1dbf0c6cd397b97
AtiPratik <pratiksamanta03@gmail.com>
2025-07-28 12:48:57 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// // FOR MENUBAR
+// let line1=document.querySelector(".line1");
+// let line2=document.querySelector(".line2");
+// let line3=document.querySelector(".line3");
+// let lines=document.querySelector(".lines");
+// let menu=document.querySelector(".main .navbar nav .nav .nav_hidden_menu ul");
+// lines.onclick=function(){
+//     if(line2.style.display=="block"){
+//     line1.style.transition=".4s";
+//     // line2.style.transition=".4s";
+//     line3.style.transition=".4s";
+//     line1.style.transform="rotate(45deg)";
+//     // line2.style.display="none";
+//     line3.style.transform="rotate(-45deg)";
+//     menu.style.display="block";
+//     }
+//     else{
+//     line1.style.transition=".4s";
+//     // line3.style.transition=".4s";
+//     line3.style.transition=".4s";
+//     line1.style.transform="rotate(0deg)";
+//     line2.style.display="block";
+//     line3.style.transform="rotate(0deg)";
+//     menu.style.display="none";
+//     }
+// }
